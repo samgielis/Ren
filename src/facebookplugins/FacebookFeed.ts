b94e7f8b227d787cc4d0240701dfc7b39c690cb8
Sam Gielis <samgielis@gmail.com>
2020-08-04 18:17:33 +0200

    Integrate new newsfeed proxy

--- src/facebookplugins/FacebookFeed.ts
+++ src/facebookplugins/FacebookFeed.ts
@@ -1,60 +1,71 @@
-import {FBFeedResponseObject, FBPostResponse} from "./IFBResponse";
-import {Loadable} from "../Loadable";
-import {FacebookProxy} from "./FacebookProxy";
-import {FacebookPost} from "./FacebookPost";
-import {parseJSON} from "../util/JSONUtils";
-import {manualFacebookFeed} from "./ManualFacebookFeed";
+import { FBFeedResponseObject, FBPostResponse } from "./IFBResponse";
+import { Loadable } from "../Loadable";
+import { FacebookProxy } from "./FacebookProxy";
+import { FacebookPost } from "./FacebookPost";
+import { parseJSON } from "../util/JSONUtils";
+import { manualFacebookFeed } from "./ManualFacebookFeed";
 
 export class FacebookFeed extends Loadable {
+  private _posts: Array<FacebookPost> = [];
 
-    private _posts : Array<FacebookPost> = [];
+  constructor() {
+    super();
+  }
 
-    constructor () {
-        super();
-    }
+  public get posts(): Array<FacebookPost> {
+    return this._posts;
+  }
 
-    public get posts () : Array<FacebookPost> {
-        return this._posts;
-    }
-
-    // Called by super();
-    protected doLoad () : void {
-        FacebookProxy.feed((res : FBFeedResponseObject) => {
-            if (!res.error && res.feed && res.feed.data) {
-                this.addPostsFromResponse(res.feed.data)
-            } else if (!res.error && parseJSON(<any>res) && parseJSON(<any>res).feed && parseJSON(<any>res).feed.data) {
-                this.addPostsFromResponse(parseJSON(<any>res.feed.data));
-            } else {
-                this.addPostsFromResponse(manualFacebookFeed);
-            }
-        }, () => {
-            this.addPostsFromResponse(manualFacebookFeed);
-        });
-    }
-
-    private addPostsFromResponse (res : FBPostResponse[]) : void {
-        for (let post of res){
-            this._posts.push(new FacebookPost(post));
+  // Called by super();
+  protected doLoad(): void {
+    FacebookProxy.feed(
+      (res: FBFeedResponseObject) => {
+        if (!res.error && res.data) {
+          this.addPostsFromResponse(res.data);
+        } else if (
+          !res.error &&
+          parseJSON(<any>res) &&
+          parseJSON(<any>res).feed &&
+          parseJSON(<any>res).feed.data
+        ) {
+          this.addPostsFromResponse(parseJSON(<any>res.data));
+        } else {
+          this.addPostsFromResponse(manualFacebookFeed);
         }
-        this.loadSuccess();
-    }
+      },
+      () => {
+        this.addPostsFromResponse(manualFacebookFeed);
+      }
+    );
+  }
 
-    public get view () : HTMLElement[] {
-        let view : HTMLElement[] = [];
-
-        for (let i = 0, displayingPosts = 0; displayingPosts < Math.min(this.posts.length, 5); i++) {
-            let post = this.posts[i];
-            if (post.canDisplay) {
-                view.push(post.view);
-                displayingPosts++;
-            }
-        }
-        return view;
+  private addPostsFromResponse(res: FBPostResponse[]): void {
+    for (let post of res) {
+      this._posts.push(new FacebookPost(post));
     }
+    this.loadSuccess();
+  }
 
-    public renderTo (parent : HTMLElement) {
-        for (let postView of this.view) {
-            parent.appendChild(postView);
-        }
+  public get view(): HTMLElement[] {
+    let view: HTMLElement[] = [];
+
+    for (
+      let i = 0, displayingPosts = 0;
+      displayingPosts < Math.min(this.posts.length, 5);
+      i++
+    ) {
+      let post = this.posts[i];
+      if (post.canDisplay) {
+        view.push(post.view);
+        displayingPosts++;
+      }
     }
+    return view;
+  }
+
+  public renderTo(parent: HTMLElement) {
+    for (let postView of this.view) {
+      parent.appendChild(postView);
+    }
+  }
 }
--- src/facebookplugins/IFBResponse.ts
+++ src/facebookplugins/IFBResponse.ts
@@ -1,32 +1,28 @@
 export interface FBResponse {
-    error : any;
+  error: any;
 }
 
 export interface FBHoursResponse extends FBResponse {
-    hours : {[moment : string] : string};
-    pageid : string;
+  hours: { [moment: string]: string };
+  pageid: string;
 }
 
 export interface FBFeedResponseObject extends FBResponse {
-    feed : FBFeedResponse;
-}
-
-export interface FBFeedResponse extends FBResponse {
-    data : Array<FBPostResponse>;
+  data: Array<FBPostResponse>;
 }
 
 export interface FBPostResponse extends FBResponse {
-    title? : string;
-    created_time : string;
-    full_picture : string;
-    id : string;
-    is_hidden : boolean;
-    is_published : boolean;
-    message : string;
-    from : FBAuthorResponse;
+  title?: string;
+  created_time: string;
+  full_picture: string;
+  id: string;
+  is_hidden: boolean;
+  is_published: boolean;
+  message: string;
+  from: FBAuthorResponse;
 }
 
 export interface FBAuthorResponse extends FBResponse {
-    name : string;
-    id : string;
+  name: string;
+  id: string;
 }
