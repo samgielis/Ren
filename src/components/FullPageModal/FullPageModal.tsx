6e7f7e09d174fa65eec741609d40d102d14a14c2
Sam Gielis <samgielis@gmail.com>
2020-08-27 19:47:39 +0200

    Add modal with image to homepage

--- src/components/FullPageModal/FullPageModal.tsx
+++ src/components/FullPageModal/FullPageModal.tsx
@@ -1,49 +1,47 @@
 import * as React from "react";
 
 export interface FullPageModalProps {
-    text: string
+  text: string;
 }
 
 export interface FullPageModalState {
-    showing: boolean
+  showing: boolean;
 }
 
-export class FullPageModal extends React.Component<FullPageModalProps, FullPageModalState> {
+export class FullPageModal extends React.Component<
+  FullPageModalProps,
+  FullPageModalState
+> {
+  componentWillMount(): void {
+    this.setState({ showing: true });
+  }
 
-    componentWillMount(): void {
-        this.setState({showing: true});
+  hide = () => {
+    this.setState({ showing: false });
+  };
+
+  render() {
+    if (!this.state.showing) {
+      return <div></div>;
     }
 
-    hide = () => {
-        this.setState({showing: false});
-    };
-
-    render() {
-        if (!this.state.showing) {
-            return <div></div>
-        }
-
-        return <div className='ren-temporary-warning'>
-            <div className='ren-temporary-warning-content'>
-                <p>
-                    Beste klanten.
-                </p>
-                <p>
-                    In het belang van de gezondheid van zowel personeel als klanten, heb ik besloten om de winkel
-                    voorlopig te sluiten tot en met 5 april. Wel kan je ons contacteren om bepaalde bestellingen op te sturen via de post. Ook onze outlet shop blijft open.
-                </p>
-                <p>
-                    <a href={"https://shop.rensport.be/pages/aanbod-tijdens-corona-crisis"}>Lees hier meer.</a>
-                </p>
-                <p>
-                    Bedankt voor jullie begrip.
-                </p>
-                <p>
-                    Groetjes, Christel.
-                </p>
-                <button onClick={this.hide}>OK</button>
-
-            </div>
+    return (
+      <div className="ren-temporary-warning">
+        <div className="ren-temporary-warning-content">
+          <div
+            className="ren-modal-close"
+            onClick={this.hide}
+            style={{
+              cursor: "pointer",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <span>X</span>
+          </div>
+          <img src="/img/ddd.jpg" width="100%" />
         </div>
-    }
+      </div>
+    );
+  }
 }
--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,23 +1,27 @@
 import * as React from "react";
-import {Navbar, NavbarProps} from "./Navbar";
-import {FullPageModal} from "../FullPageModal/FullPageModal";
+import { Navbar, NavbarProps } from "./Navbar";
+import { FullPageModal } from "../FullPageModal/FullPageModal";
 
 export class PageHeader extends React.Component<NavbarProps, {}> {
-    render() {
-           /* <FullPageModal text={""}></FullPageModal>*/
-        return <div>
-            <MainLogo />
-            <Navbar activeContext={this.props.activeContext}/>
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <FullPageModal text={""}></FullPageModal>
+        <MainLogo />
+        <Navbar activeContext={this.props.activeContext} />
+      </div>
+    );
+  }
 }
 
 class MainLogo extends React.Component {
-    render() {
-        return <div className="row ren-main-logo-container">
-            <div className="col-md-12">
-                <img className='ren-main-logo-img' src='/img/logo.png'/>
-            </div>
+  render() {
+    return (
+      <div className="row ren-main-logo-container">
+        <div className="col-md-12">
+          <img className="ren-main-logo-img" src="/img/logo.png" />
         </div>
-    }
+      </div>
+    );
+  }
 }
