58e19e4f955f91371d6b2e0e1365dc4cb519daa9
Sam Gielis <samgielis@gmail.com>
2024-03-12 20:40:51 +0100

    Newsfeed square images

--- src/screens/Home/Newsfeed.tsx
+++ src/screens/Home/Newsfeed.tsx
@@ -1,12 +1,14 @@
 import {
+  AspectRatio,
   Box,
   Button,
   Card,
-  Center,
   GridItem,
   Heading,
+  HStack,
   Image,
   SimpleGrid,
+  Spacer,
   Stack,
   Text,
   useBreakpointValue,
@@ -15,7 +17,7 @@ import { format } from "date-fns/format";
 import { nlBE } from "date-fns/locale";
 import { setDefaultOptions } from "date-fns/setDefaultOptions";
 import { useEffect, useState } from "react";
-import { FaFacebook } from "react-icons/fa";
+import { FaExternalLinkAlt, FaFacebook } from "react-icons/fa";
 import { ExternalLink } from "../../components/Link/Link";
 
 type NewsFeedPost = {
@@ -45,7 +47,7 @@ export const Newsfeed = () => {
 
   return (
     <Stack bgColor="accent.100" p={10} spacing={10}>
-      <Box>
+      <HStack>
         <Heading
           size="md"
           bg="dark"
@@ -56,11 +58,21 @@ export const Newsfeed = () => {
         >
           Updates
         </Heading>
-      </Box>
+        <Spacer />
+        <Button
+          as={ExternalLink}
+          href="https://www.facebook.com/rentessenderlo"
+          colorScheme={"accent"}
+          size="lg"
+          rightIcon={<FaFacebook />}
+        >
+          Volg ons op Facebook
+        </Button>
+      </HStack>
       <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 2, "2xl": 3 }}
-        columnGap={10}
-        rowGap={10}
+        columns={{ base: 1, md: 2, lg: 3, "2xl": 4 }}
+        columnGap={5}
+        rowGap={5}
         alignItems="stretch"
       >
         {data.data
@@ -72,17 +84,6 @@ export const Newsfeed = () => {
             </GridItem>
           ))}
       </SimpleGrid>
-      <Center>
-        <Button
-          as={ExternalLink}
-          href="https://www.facebook.com/rentessenderlo"
-          colorScheme={"accent"}
-          size="lg"
-          rightIcon={<FaFacebook />}
-        >
-          Volg ons op Facebook
-        </Button>
-      </Center>
     </Stack>
   );
 };
@@ -91,27 +92,63 @@ const NewsFeedPostCard = ({ post }: { post: NewsFeedPost }) => {
   setDefaultOptions({ locale: nlBE });
 
   return (
-    <Card p={6} h="full" bgSize="cover">
+    <Card p={5} h="full" bgSize="cover">
       <Stack
-        spacing={6}
+        h="full"
+        spacing={2}
         align="stretch"
         dir={useBreakpointValue({ base: "column", md: "row" })}
       >
-        <Stack spacing={6} flex={1}>
-          <Heading size="md" color="accent.500">
-            {format(new Date(post.created_time), "PPP")}
-          </Heading>
+        <Stack spacing={4} flex={1}>
+          <HStack>
+            <Heading size="md" color="accent.500">
+              {format(new Date(post.created_time), "PPP")}
+            </Heading>
+            <Spacer />
+            <Button
+              size="sm"
+              variant="ghost"
+              as={ExternalLink}
+              href={post.permalink_url}
+              colorScheme="accent"
+              rightIcon={<FaExternalLinkAlt />}
+            >
+              bekijk post
+            </Button>
+          </HStack>
           <Text>{post.message}</Text>
         </Stack>
-        {post.full_picture && (
-          <Image
-            borderRadius={"lg"}
-            w={useBreakpointValue({ base: "100%", md: "100%" })}
-            src={post.full_picture}
-            objectFit={"contain"}
-          />
-        )}
+        <Spacer />
+        <AlwaysSquareImage src={post.full_picture} />
       </Stack>
     </Card>
+  );
+};
+
+const AlwaysSquareImage = ({ src }: { src: string }) => {
+  return (
+    <Box bg="accent.100" borderRadius={"lg"} overflow="hidden">
+      <AspectRatio ratio={1 / 1}>
+        <Box pos="relative">
+          <Image
+            pos="absolute"
+            src={src}
+            w="full"
+            h="full"
+            filter="blur(10px)"
+            objectFit={"cover"}
+            opacity={0.5}
+          />
+          <Image
+            pos="absolute"
+            zIndex={2}
+            objectFit={"contain"}
+            src={src}
+            w="full"
+            h="full"
+          />
+        </Box>
+      </AspectRatio>
+    </Box>
   );
 };
